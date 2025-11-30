7522aef2ce8a0c3953f9005fb198941d8b912f96
stanley-hypo <stanley@hypoidea.com>
2025-11-30 18:47:25 +0000

    feat: Remove "Create account" link from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,12 +10,7 @@ export default function LoginPage() {
   return (
     <Container size={420} my={40}>
       <Title ta="center">Welcome back!</Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Text component="a" href="#" size="sm" c="blue">
-          Create account
-        </Text>
-      </Text>
+
 
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
         <form action={dispatch}>
